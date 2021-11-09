0b19444a116eb969c27aa27861d3f7be90b33a30
Jaime Uribe <jauribe@condorlabs.io>
2021-11-08 20:27:35 -0500

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Jaime Uribe on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
